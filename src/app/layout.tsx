22e6316d2684f35fe40a13f0e9a425b138e04129
Kawan02 <kawanng39@gmail.com>
2025-01-25 18:41:36 -0300

    feat: inclusão do favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt" className={inter.className}>
+      <body>{children}</body>
     </html>
   )
 }
