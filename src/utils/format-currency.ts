815dd8ac03a4b15cd3c7e07b1aee7e7a411f6a22
Kawan02 <kawanng39@gmail.com>
2025-01-26 11:22:24 -0300

    feat: função para formatar o valor para currency

---
+++ src/utils/format-currency.ts
@@ -0,0 +1,8 @@
+export function formatToCurrency(price: number): string {
+  return price.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
